5f8b7477f6769394d8d97706dcc6c0e1958dcbad
Thingk0 <audtjd9080@naver.com>
2022-10-11 19:58:29 +0900

    PeakTime project by.thingk0

---
+++ src/test/java/org/peaktime/PeakTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.peaktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
